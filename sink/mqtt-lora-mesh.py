eb8a77f5a3b5a8de48780c9491e019db2d1bfb99
hanwckf <my375229675@gmail.com>
2020-01-29 08:20:18 +0000

    add sink python mqtt script

---
+++ sink/mqtt-lora-mesh.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+import time
+import sys
+import serial
+import re
+import json
+import paho.mqtt.client as mqtt
+from threading import Thread
+from queue import Queue
+
+Tlink_SN = ""
+USBserial = "/dev/ttyUSB0"
+BaudRate = 115200
+
+def on_connect(client, userdata, flags, rc):
+	print("mqtt connection return: " + str(rc))
+
+def serial_read(s, q):
+	pattern = re.compile(r'__upd__\[(\d+)\]\[(\-?\d+\.\d+)\]\[(\d+)\]')
+	while (serial_alive):
+		b = s.readline()
+		if b:
+			b = b.decode('utf-8')
+			r = pattern.findall(b)
+			if r:
+				q.put(r[0])
+	print("serial thread exit")
+
+def data2json(r):
+	temp = { "flag": "temp_" + r[0], "value": float(r[1]) }
+	volt = { "flag": "volt_" + r[0], "value": int(r[2]) }
+	upload_data = { "sensorDatas": [ temp, volt ] }
+	return json.dumps(upload_data)
+
+if __name__ == '__main__':
+	argc = len(sys.argv)
+	if argc < 2:
+		print ("please specify TlinkSN")
+		sys.exit(1)
+	Tlink_SN = sys.argv[1]
+	if argc >= 3:
+		USBserial = sys.argv[2]
+		if argc >= 4:
+			BaudRate = int(sys.argv[3])
+	print("Serial port: " + USBserial)
+	print("BaudRate: " + str(BaudRate))
+	try:
+		tty = serial.Serial(USBserial, BaudRate, timeout=3)
+	except:
+		print("Serial port open failed!")
+		sys.exit(1)
+
+	client = mqtt.Client(client_id=Tlink_SN)
+	client.on_connect = on_connect
+	client.username_pw_set("MQTT", password="MQTTPW")
+
+	client.connect("mq.tlink.io", 1883, 60)
+	client.loop_start()
+
+	q = Queue()
+	serial_alive = True
+	t = Thread(target=serial_read, args=(tty,q,))
+	t.start()
+
+	try:
+		while True:
+			data = q.get(block=True, timeout=None)
+			j = data2json(data)
+			ret = client.publish(Tlink_SN, payload=j, qos=0)
+			if ret.rc == mqtt.MQTT_ERR_NO_CONN:
+				print("mqtt connection fail")
+			elif ret.rc != mqtt.MQTT_ERR_SUCCESS:
+				print("mqtt publish fail")
+
+	except KeyboardInterrupt:
+		client.loop_stop()
+		serial_alive = False
+		t.join()
